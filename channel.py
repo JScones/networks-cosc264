6fc585ebb1c771e4a6f8ba34bd652303d700b608
Joshua <jbe113@uclive.ac.nz>
2017-08-07 21:33:32 +1200

    Added the basic python files for the assignment. All have the same imports and just print out their command line arguments.

---
+++ channel.py
@@ -0,0 +1,14 @@
+""" Channel program for Cosc264 Assignment
+
+    Does nothing yet.
+
+    Authors: Josh Bernasconi 68613585
+             James Toohey    xxxxxx
+"""
+
+import socket
+import sys
+
+
+if __name__ == '__main__':
+    print(sys.argv)
